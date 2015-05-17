8fd9cf21872fbc3851a5b5d7c76c2dc4166e6ad8
Dhaval <trivedi.dhaval@gmail.com>
2015-05-17 20:55:51 +0530

    Added remove function

--- binary_search_tree.js
+++ binary_search_tree.js
@@ -48,6 +48,86 @@ BinarySearchTree.prototype.add = function(value) {
     }
 };
 
+BinarySearchTree.prototype.remove = function(value) {
+    var found = false,
+        parent = null,
+        current = this._root,
+        childCount,
+        replacementParent = null,
+        replacement;
+    while (!found && current) {
+        if (value < current.value) {
+            parent = current;
+            current = current.left;
+        } else if (value > current.value) {
+            parent = current;
+            current = current.right;
+        } else {
+            found = true;
+        }
+    }
+    if (found) {
+        childCount = (current.left === null ? 0 : 1) + (current.right === null ? 0 : 1);
+        if (current === this._root) {
+            switch (childCount) {
+                case 0:
+                    this._root = null;
+                    break;
+                case 1:
+                    this._root = (current.left === null ? current.right : current.left);
+                    break;
+                case 2:
+                    replacement = this._root.left;
+                    while (replacement.right !== null) {
+                        replacementParent = replacement;
+                        replacement = replacement.right;
+                    }
+                    if (replacementParent !== null) {
+                        replacmentParent.right = replacement.left;
+                        replacement.left = this._root.left;
+                        replacement.right = this._root.right;
+                    } else {
+                        replacement.right = this._root.right;
+                    }
+                    this._root = replacement;
+            }
+        } else {
+            switch (childCount) {
+                case 0:
+                    if (current.value < parent.value) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                    break;
+                case 1:
+                    var child = (current.left === null ? current.right : current.left);
+                    if (current.value < parent.value) {
+                        parent.left = child;
+                    } else {
+                        parent.right = child;
+                    }
+                    break;
+                case 2:
+                    replacement = current.left;
+                    replacementParent = current;
+                    while (replacement.right !== null) {
+                        replacementParent = replacement;
+                        replacement = replacement.right;
+                    }
+                    replacementParent.right = replacement.left;
+                    replacement.left = current.left;
+                    replacement.right = current.right;
+                    if (current.value < parent.value) {
+                        parent.left = replacement;
+                    } else {
+                        parent.right = replacement;
+                    }
+            }
+        }
+    }
+};
+
 BinarySearchTree.prototype.traverse = function(action) {
     function inOrder(node) {
         if (node) {
